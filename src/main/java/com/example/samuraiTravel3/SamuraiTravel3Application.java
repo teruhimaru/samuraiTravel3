8747aaf7ee4bf75c8eb0b3bab8de70ef10ac02f5
Hikaru_Nakahashi <nanan@192.168.10.139>
2025-02-07 16:23:54 +0900

    最初のコミット

---
+++ src/main/java/com/example/samuraiTravel3/SamuraiTravel3Application.java
@@ -0,0 +1,13 @@
+package com.example.samuraiTravel3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuraiTravel3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraiTravel3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samuraiTravel3/SamuraiTravel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samuraiTravel3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuraiTravel3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
