0a0bc8c3d9a75dfeb3ac3352a07929ff8e6081e0
Hikaru_Nakahashi <nanan@192.168.40.52>
2025-02-12 17:59:40 +0900

    会員登録フォーム作成まで

--- src/main/java/com/example/samuraiTravel3/controller/AuthController.java
+++ src/main/java/com/example/samuraiTravel3/controller/AuthController.java
@@ -1,12 +1,34 @@
 package com.example.samuraiTravel3.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.samuraiTravel3.form.SignupForm;
 
 @Controller
 public class AuthController {
-	@GetMapping("login")
+	@GetMapping("/login")
 	public String login() {
 		return "auth/login";
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("signupForm", new SignupForm());
+		return "auth/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String signup(@ModelAttribute @Validated SignupForm signupForm,
+			BindingResult bindingResult,
+			RedirectAttributes redirectAttributes
+			) {
+		
 	}
 }
